f087a8102807023b9ddd9d224ff8912a6f5dafa2
Ihor Harkavyi <igor07102002@gmail.com>
2023-07-08 13:24:56 +0200

    FragmentMenu done

--- app/src/main/java/com/example/seemymessage/fragments/FragmentMenu.kt
+++ app/src/main/java/com/example/seemymessage/fragments/FragmentMenu.kt
@@ -5,15 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.seemymessage.R
+import com.example.seemymessage.databinding.FragmentMenuBinding
+import com.example.seemymessage.databinding.FragmentSplashBinding
 
 class FragmentMenu : Fragment() {
+
+    private var _binding: FragmentMenuBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        return inflater.inflate(R.layout.fragment_menu, container, false)
+        _binding = FragmentMenuBinding.inflate(layoutInflater)
+        return (binding.root)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.startBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_fragmentMenu_to_fragmentTextSettings)
+        }
     }
 }
